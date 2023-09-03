a6fc983120e7c2d384cca74032bb98505905b1f5
Peredoro <phtaborda8@gmail.com>
2023-09-03 09:00:41 -0300

    inserção dos arquivos feitos remotamente no local dos que estavam no repositorio

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -1,31 +1,38 @@
 
 const pokeApi = {}
-
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    const stats = pokeDetail.stats.map(stat => stat.base_stat)
+    pokemon.stats = stats
+
+    function capitalize(text){
+        return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+
+    const abilities = pokeDetail.abilities.map(ab => new Set().add(capitalize(ab.ability.name)).values().next().value)
+    pokemon.abilities = abilities
 
     return pokemon
 }
-
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
-
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
